326e4fcc7b82afd2701a2f5f46d433ba6b99497b
Mostafa-Alsadawy <mostafa.alsadawy2021@gmail.com>
2022-04-12 02:30:54 +0200

    before testing

--- src/handlers/order.handler.ts
+++ src/handlers/order.handler.ts
@@ -17,4 +17,64 @@ const ordersRouts = (app:express.Application)=>{
         } catch (error) {
             next(error)
         }
-    }
+    });
+
+    // Route to get one order 
+    app.get("/orders/:id",async(req:express.Request,res:express.Response,next:NextFunction):Promise<void>=>{
+        try {
+            const id = parseInt(req.params.id);
+            const result = await orderModel.show(id);
+            res.json({
+                status:"success",
+                message:"done list one ordere with id "+id,
+                data:result
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+    // Route to create new order 
+    app.post("/orders/create",async(req:express.Request,res:express.Response,next:NextFunction):Promise<void>=>{
+        try {
+            const result = await orderModel.create(req.body);
+            res.json({
+                status:"success",
+                message:"done create one ordere with id "+req.body.id,
+                data:result
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+    // Route to update order
+
+    app.put("/orders/update",async(req:express.Request,res:express.Response,next:NextFunction):Promise<void>=>{
+        try {
+            const result = await orderModel.update(req.body);
+            res.json({
+                status:"success",
+                message:"done update one ordere with id "+req.body.id,
+                data:result
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+
+    // Route to delete existing order 
+    app.delete("/orders/:id",async(req:express.Request,res:express.Response,next:NextFunction):Promise<void>=>{
+        try {
+            const id = parseInt(req.params.id);
+            const result = await orderModel.delete(id);
+            res.json({
+                status:"success",
+                message:"done delete one ordere with id "+id,
+                data:result
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+}
--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -7,11 +7,11 @@ export type Order = {
 };
 
 export type OrderProduct = {
-    id?:number,
-    quantity:number,
-    orderId:number,
-    productId:number
-}
+  id?: number;
+  quantity: number;
+  orderId: number;
+  productId: number;
+};
 
 export class OrderModel {
   // get all orders
@@ -54,23 +54,66 @@ export class OrderModel {
         throw new Error("this is item does not exist");
       }
       const sql = "DELETE FROM orders WHERE id = $1 RETUENING *;";
-      const result = await connection.query(sql,[id]);
+      const result = await connection.query(sql, [id]);
       return result.rows[0];
     } catch (error) {
       throw new Error((error as Error).message);
     }
   }
 
-  // add new product to order 
-  async addProduct(OrderProduct:OrderProduct):Promise<OrderProduct>{
-    try{
-        const connection = await client.connect();
-        const sql = "INSERT INTO order_products (quantity,user_id,product_id) VALUES ($1,$2,$3) RETUENING*;";
-        const result = await connection.query(sql,[OrderProduct.quantity,OrderProduct.orderId,OrderProduct.productId])
-        return result.rows[0];
-    }catch(err){
-        throw new Error((err as Error).message);
-    }
+  // create new order
 
+  async create(order: Order): Promise<Order> {
+    try {
+      const connenction = await client.connect();
+      const sql =
+        "INSERT INTO orders (stat,user_id) VALUES($1,$2) RETUENING *;";
+      const result = await connenction.query(sql, [order.stat, order.userId]);
+      connenction.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error((error as Error).message);
+    }
+  }
+  // update an existing order (open or closed)
+
+  async update(order: Order): Promise<Order> {
+    try {
+      const connenction = await client.connect();
+
+      //check if the product exists
+      const inDB = await connenction.query(
+        "SELECT id from orders WHERE id = $1;",
+        [order.id]
+      );
+      if (inDB.rowCount <= 0) {
+        throw new Error("this order does not exist");
+      }
+
+      const sql =
+        "UPDATE  orders SET (stat,user_id) VALUES($1,$2) RETUENING *;";
+      const result = await connenction.query(sql, [order.stat, order.userId]);
+      connenction.release();
+      return result.rows[0];
+    } catch (error) {
+      throw new Error((error as Error).message);
+    }
+  }
+
+  // add new product to order
+  async addProduct(OrderProduct: OrderProduct): Promise<OrderProduct> {
+    try {
+      const connection = await client.connect();
+      const sql =
+        "INSERT INTO order_products (quantity,user_id,product_id) VALUES ($1,$2,$3) RETUENING*;";
+      const result = await connection.query(sql, [
+        OrderProduct.quantity,
+        OrderProduct.orderId,
+        OrderProduct.productId,
+      ]);
+      return result.rows[0];
+    } catch (err) {
+      throw new Error((err as Error).message);
+    }
   }
 }
